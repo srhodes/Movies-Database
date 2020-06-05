ad527cf7be12523fa955007abd5b5675e23d5d5b
srhodes <rhodestephane@comcast.net>
2020-06-04 18:38:48 -0700

    Updates

---
+++ precache-manifest.1012f2a84ff1c6a288380e2084d62e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264449fcf613cfb7183f710e7b434fd6",
+    "url": "/Movies-Database/index.html"
+  },
+  {
+    "revision": "9a04e71d0521e9ae2bec",
+    "url": "/Movies-Database/static/css/main.89de7b32.chunk.css"
+  },
+  {
+    "revision": "0dfcf97381de3754958d",
+    "url": "/Movies-Database/static/js/2.35b725d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-Database/static/js/2.35b725d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a04e71d0521e9ae2bec",
+    "url": "/Movies-Database/static/js/main.fbdcc849.chunk.js"
+  },
+  {
+    "revision": "ccfb13f16ce09600f746",
+    "url": "/Movies-Database/static/js/runtime-main.434c0a73.js"
+  }
+]);
